94048f307d02e7ec552f9b88699a93f23084f78b
Jun Kai <kuhn96@gmail.com>
2016-05-03 01:03:04 +0000

    Now it will change ip address when it is not connected to master

--- check2.py
+++ check2.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(0.5)
@@ -6,7 +7,10 @@ s.settimeout(0.5)
 try:
   s.connect(('192.168.1.2', 3306))
   print 'Connected'
+  # ip addr flush wlan0
 except Exception, e:
   print 'Not connected'
-
+  os.system('sudo ifconfig wlan0 down')
+  os.system('sudo ifconfig wlan0 192.168.1.2')
+  os.system('sudo ifconfig wlan0 up')
 s.close()
